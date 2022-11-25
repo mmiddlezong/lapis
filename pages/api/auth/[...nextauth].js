907c3d933f851d929ebd93ec3e10aa6854203bfb
mmiddlezong <mmiddlezong@gmail.com>
2022-11-25 16:14:08 -0500

    add google auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google"
+
+const authOptions = {
+    session: {
+        strategy: "jwt"
+    },
+    providers: [
+        
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    pages: {
+        //signIn: "/auth/signin",
+    }
+}
+
+export default NextAuth(authOptions)
