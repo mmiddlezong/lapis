6035ecbd37ed2f0c0227d67b3d763e9ba7903fc6
mmiddlezong <mmiddlezong@gmail.com>
2022-11-25 00:18:40 -0500

    bootstrap works now!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
 function Home() {
-  return <h1>Lapis</h1>
+  return <>
+    <div class="row my-5 bg-transparent align-items-center gx-5 justify-content-evenly">
+      <div class="col-sm-5">
+      </div>
+      <div class="col-sm-4">
+        <h1 class="display-3">The Most Time-Efficient Way to Study</h1>
+      </div>
+    </div>
+    
+    <div class="p-4 bg-light">
+      <div class="text-start">
+        <p class="fs-3 fw-bold">Why Lapis?</p>
+      </div>
+      <div class="row justify-content-evenly align-items-center">
+        <div class="col-sm-3">
+            <p class="my-3">Lapis has been highly regarded by Innovation Foundry.</p>
+        </div>
+        <div class="col-sm-3">
+            <p class="my-3">Lapis has been shown to Improve Test Results by 25%.</p>
+        </div>
+        <div class="col-sm-3 ">
+            <p class="my-3">Lapis is Science Based: The Pomodoro Technique & Spaced Repetition.</p>
+        </div>
+      </div>
+    </div>
+  </>
 }
 
 export default Home
