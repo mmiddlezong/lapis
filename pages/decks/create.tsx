88a185c3eca82770b8b9467f72b6109309719fe8
Chang <changyoonkim26@gmail.com>
2022-12-04 21:53:46 -0500

    micahel

--- pages/decks/create.tsx
+++ pages/decks/create.tsx
@@ -2,19 +2,23 @@ export default function Create() {
     return (<>
 
         <div className="container">
-            <div className="col">
+            <div className="row justify-content-end">
+                
+              <div className="col-4">
                 <div>
-                    <div className= "w-50">
+                    <div className= "w-100">
                         <label htmlFor="title" className="form-label"></label>
-                        <textarea placeholder="Front" rows="13" id="title" maxLength="200" className='form-control' type='text'></textarea>
+                        <textarea className= "chang-cool" placeholder="Front" rows="13" id="title" maxLength="200" className='form-control' type='text'>.bg-light</textarea>
                     </div>
-                    <div className= "w-50">
+                    <div className= "w-100">
                         <label htmlFor="title" className="form-label"></label>
-                        <textarea placeholder="Back" rows="13" id="title" maxLength="200" className='form-control' type='text'></textarea>
+                        <textarea placeholder="Back" rows="13" id="title" maxLength="200" className='form-control' type='text'>.bg-light</textarea>
                     </div>
+                    
                     <button type="submit" className="btn btn-primary mb-3">Submit</button>
 
                 </div>
+            </div>
             </div>
         </div>
 
